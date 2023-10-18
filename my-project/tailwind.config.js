6aad24e13b6678d29f986d1093a05b925d87bb03
ernest <gyeduernest@gmail.com>
2023-10-18 17:31:58 +0000

    Flowbite insatalled

---
+++ my-project/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+
+
+  ],
+  theme: {
+    
+    extend: {
+      screens: {
+        'sm': '375px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      }
+    },
+  },
+  plugins: [],
+}
+
