817f09ba26577d636a3481cb34a28520e1835188
ernest <gyeduernest@gmail.com>
2023-10-19 00:01:26 +0000

    Json object data file added

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -9,13 +9,7 @@ export default {
   theme: {
     
     extend: {
-      screens: {
-        'sm': '375px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px',
-      }
+      
     },
   },
   plugins: [],
